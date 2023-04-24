0d734d75681dfc8cb847dca27d63d81c7f8f389d
Jeanderson Amaral <112119010+JeandersonAmaral@users.noreply.github.com>
2023-04-24 14:50:15 -0300

    Atualização do sistema

---
+++ algoritmo.js
@@ -0,0 +1,12 @@
+function loginSenha() {
+    var login = document.querySelector('#login').value;
+    var senha = document.querySelector('#senha').value;
+
+    if ((login === 'Jeanderson' && senha === '123') || (login === 'Tamyres' && senha === '12345')) {
+        window.location.href = "./home.html";
+    } else {
+        alert('Usuário ou senha incorretos.\nTente novamente!');
+    }
+}
+
+
